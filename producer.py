5924814a97f18cea9a1e3c72e8cacb84fb5cc779
luluagasantiago <luluagasantiago@icloud.com>
2023-12-24 00:47:32 +0100

    added error handling and writing JSON with the last 10 posts titles

--- producer.py
+++ producer.py
@@ -1,16 +1,22 @@
 import requests
 import json 
 
-url = "https://api.reddit.com/r/dancarlin/"
-def get_json(url):
+def get_json(url, data):
     r = requests.get(url)
-    print(r.json())
-    print(r.status_code)
-    print("THE END")
+    
+    #In case the JSON decoding fails, r.json() raises an exception
+    try:
+       
+        print(r.raise_for_status())
+        with open(data, 'w+') as f:
+            json.dump(r.json(), f )
+        return r.status_code
+    except Exception as e:
+        return r.status_code
 
 
 def parse_json(json_file):
-    with open(json_file) as f:
+    with open(json_file, "r") as f:
         data = json.load(f)   
         children = data['data']['children']
         #print(children)
@@ -20,7 +26,13 @@ def parse_json(json_file):
             count += 1
 
 def main():
-    parse_json('api.reddit.com.json')
+    url = "https://api.reddit.com/r/dancarlin/"
+    file = "new_file.json"
+    code_request = get_json(url, file)
+    if code_request == 200:
+        parse_json(file)
+    else:
+        print("there was a problem")
 
 if __name__ == "__main__":
     main()
